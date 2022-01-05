541ec1dfe30d6e575fff9a285580d4b71621f5d6
baristakat <bcarey0803@gmail.com>
2022-01-04 22:45:21 -0500

    adding variables and color

--- Module 3/src/Opt2UI.java
+++ Module 3/src/Opt2UI.java
@@ -1,5 +1,20 @@
-import java.awt.*;
+import javax.swing.*;
 
-public class Opt2UI {
-    // this is a test
+public class Opt2UI extends JFrame {
+    // initiate frame
+    JFrame orangeApp;
+    JPanel back = new JPanel();
+    JLabel header;
+    JButton dateTime;
+    JButton Save;
+    JButton orange;
+    Color tangerine = new Color(255, 153, 0);
+    JButton exit;
+
+    Opt2UI() {
+        orangeApp = new JFrame();
+        orangeApp.add(back);
+
+        // header and variables
+    }
 }
